701b46a1b3f998cf0da4b000d9da4c144d67c6f7
diamondHoe420 <stachulski122@gmail.com>
2020-12-19 18:44:57 +0100

    wprowadzono pobieranie dni i ich opisów z kalendarza

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,49 @@ public class Main {
 
         Elements days = doc.getElementsByClass("festCal_d");
         for (Element el : days) {
-            System.out.println(el.getElementsByTag("a").attr("title"));
+            String pUrl = el.getElementsByTag("a").attr("href");
+            if (pUrl=="") {
+                continue;
+            }
+            HandleDateData(pUrl);
         }
+    }
+
+    public static void HandleDateData(String url) {
+        Document doc;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            System.out.println("COULD NOT CONNECT TO " + url);
+            return;
+        }
+
+
+        try {
+            Element events = doc.getElementsByClass("calCard-ententa").first();
+            for (Element event : events.getElementsByTag("a") ) {
+                System.out.println(event.text());
+                String desc = GetEventDescription( event.attr("href") );
+                if (desc!="")
+                    System.out.println(desc);
+            }
+        } catch (NullPointerException e) {
+            return;
+        }
+    }
+
+    public static String GetEventDescription(String url) {
+        Document doc;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            System.out.println("COULD NOT CONNECT TO " + url);
+            return "";
+        }
+        Element el = doc.getElementById("opis_nt");
+        if (el!=null) {
+            return el.text();
+        }
+        return "";
     }
 }
